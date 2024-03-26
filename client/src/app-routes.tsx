d22186dc6c0a9b1bc92bbe08b6e58bcde5cd7a85
green <macbook@macs-MacBook-Pro.local>
2024-03-26 10:22:45 +0100

    layout setup

--- client/src/app-routes.tsx
+++ client/src/app-routes.tsx
@@ -1,9 +1,10 @@
 
 import {Routes,Route, Navigate} from 'react-router-dom';
+import Layout from './layouts/layout';
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/" element={<Layout>Home Page</Layout>} />
             <Route path="/profile" element={<h1>Profile</h1>} />
             <Route path="*" element={<Navigate to={'/'}/>} />
         </Routes>
