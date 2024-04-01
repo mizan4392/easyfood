4ed337232ddcd0aed3e32dfb1d153e1ae1b21f4b
green <macbook@macs-MacBook-Pro.local>
2024-04-01 19:29:24 +0200

    create-user-api

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Auth0Provider ,AppState,User} from "@auth0/auth0-react";
+import { useCreateMyUser } from "@/apis/users";
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Auth0ProviderWithNavigate({ children }: Props) {
+
+  const {createUser} = useCreateMyUser()
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
@@ -17,6 +20,9 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
 
   const onRedirectCallback = (appState?: AppState,user?:User) => {
     console.log("User",user)
+    if(user?.sub && user?.email){
+      createUser({auth0Id:user.sub,email:user.email,name:user.name})
+    }
   }
 
   return (
