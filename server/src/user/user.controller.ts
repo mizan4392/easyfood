69d0de2f703e8a4e3b2c71e68745b65ed8a3fc16
green <macbook@macs-MacBook-Pro.local>
2024-04-04 20:52:15 +0200

    auth0-api-token-validation

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './user.dto';
+import { Auth0AuthorizationGuard } from 'src/authorization/auth0-authorization.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('create')
+  @UseGuards(Auth0AuthorizationGuard)
   createUser(@Body() body: UserDto) {
     return this.userService.createUser(body);
+  }
+
+  @Get('fetch')
+  fetchUsers() {
+    return 'unprotected route';
   }
 }
