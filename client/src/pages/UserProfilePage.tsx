073600f4b57df3bf2efa8ff484125ae4f70ef171
green <macbook@macs-MacBook-Pro.local>
2024-04-08 15:49:52 +0200

    build-fix-ui-v1

--- client/src/pages/UserProfilePage.tsx
+++ client/src/pages/UserProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useGetMyUser, useUpdateMyUser } from '@/apis/users'
 import UserProfileForm from '@/forms/UserProfileForm'
-import React from 'react'
+
 
 
 export default function UserProfilePage() {
