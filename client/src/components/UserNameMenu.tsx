1d3d076e871c1e1fc345fdb09da6afdd64d03b56
green <macbook@macs-MacBook-Pro.local>
2024-06-20 15:23:30 +0200

    my-restaurant-form-v1

--- client/src/components/UserNameMenu.tsx
+++ client/src/components/UserNameMenu.tsx
@@ -1,4 +1,3 @@
-
 import { DropdownMenu, DropdownMenuContent } from "./ui/dropdown-menu";
 import {
   DropdownMenuItem,
@@ -11,22 +10,35 @@ import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 
 export default function UserNameMenu() {
-  const { user,logout } = useAuth0();
+  const { user, logout } = useAuth0();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className=" flex items-center px-3 font-bold hover:text-orange-500 gap-2">
         <CircleUserRound className=" text-orange-500" />
         {user?.email}
       </DropdownMenuTrigger>
-      <DropdownMenuContent className=" flex flex-col gap-2" >
+      <DropdownMenuContent className=" flex flex-col gap-2">
+        <DropdownMenuItem>
+          <Link
+            to={"manage-restaurant"}
+            className=" font-bold hover:text-orange-500"
+          >
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuItem>
           <Link to={"profile"} className=" font-bold hover:text-orange-500">
             User Profile{" "}
           </Link>
         </DropdownMenuItem>
         <Separator />
-        <DropdownMenuItem >
-          <Button onClick={()=>logout()} className=" flex flex-1 font-bold bg-orange-500">Log out</Button>
+        <DropdownMenuItem>
+          <Button
+            onClick={() => logout()}
+            className=" flex flex-1 font-bold bg-orange-500"
+          >
+            Log out
+          </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
---
+++ client/src/forms/manage-restaurant-form/CuisinesSection.tsx
@@ -0,0 +1,35 @@
+import { FormDescription, FormField, FormItem } from "@/components/ui/form";
+import { cuisineList } from "@/config/restaurant-options-config";
+import { useFormContext } from "react-hook-form";
+import CuisineCheckbox from "./CuisineCheckbox";
+
+export default function CuisinesSection() {
+  const { control } = useFormContext();
+  return (
+    <div className=" space-y-2">
+      <div>
+        <h2 className=" text-2xl font-bold">Cuisines</h2>
+        <FormDescription>
+          Select the cuisines that your restaurant serves
+        </FormDescription>
+      </div>
+      <FormField
+        control={control}
+        name="cuisines"
+        render={({ field }) => (
+          <FormItem>
+            <div className=" grid md:grid-cols-5 gap-1">
+              {cuisineList.map((cuisine) => (
+                <CuisineCheckbox
+                  key={cuisine}
+                  cuisine={cuisine}
+                  field={field}
+                />
+              ))}
+            </div>
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
