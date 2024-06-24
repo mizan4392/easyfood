8d76d5c00acb256316bbd0c20b8cc9469a446e8e
green <macbook@macs-MacBook-Pro.local>
2024-06-24 16:58:11 +0200

    my-restaurant-form-api-connect

--- client/src/pages/ManageRestaurantPage.tsx
+++ client/src/pages/ManageRestaurantPage.tsx
@@ -1,5 +1,9 @@
+import { useCreateMyRestaurantRequest } from "@/apis/myResturant";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurantRequest();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 }
