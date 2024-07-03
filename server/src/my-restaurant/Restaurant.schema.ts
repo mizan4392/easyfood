8d89b6b30d81688ba4d64aec0458a1ff7a879a8d
green <macbook@macs-MacBook-Pro.local>
2024-07-03 14:22:21 +0200

    build fix v2

--- server/src/my-restaurant/Restaurant.schema.ts
+++ server/src/my-restaurant/Restaurant.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
-import { User } from '../user/user.schema';
+import { User } from 'src/user/User.schema';
 
 @Schema()
 export class MenuItem {
