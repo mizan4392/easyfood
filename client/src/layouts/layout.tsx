726b1482f155160d2b2316058a3393b71342fd5f
“Mizan” <macbook@macs-MacBook-Pro.local>
2026-02-27 22:06:18 +0100

    remove waring

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Warning from "@/components/Warning";
+
 import React from "react";
 
 type Props = {
@@ -11,7 +11,6 @@ type Props = {
 function Layout({ children, showHero }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Warning />
       <Header />
       {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
